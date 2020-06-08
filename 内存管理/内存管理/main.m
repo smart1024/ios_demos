f66056445ff2be632d947213b34de7b36cc3ef26
liling <1059232202@qq.com>
2020-06-08 22:08:45 +0800

    提交修改

--- 内存管理/内存管理/main.m
+++ 内存管理/内存管理/main.m
@@ -7,11 +7,30 @@
 //
 
 #import <Foundation/Foundation.h>
+#import "ClassA.h"
 
+/**
+ 速记法：
+ strong：自定义对象、控制器对象、如果不指定也是strong
+ weak：代理对象、IBOulet使用weak
+ assign：基本数据类型，如int float和NSInteger、CGFloat等
+ copy：NSString、NSArray、NSDictionary及其可变子类、Block块推荐使用copy
+ */
+//strong、weak直接指向string地址所以地址一样，copy会在内存拷贝一个副本，所以地址不同
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+        NSMutableString* string =[[NSMutableString alloc] initWithFormat:@"normal"];
+        ClassA* classA = [[ClassA alloc] init];
+        classA.name = string;
+        classA.strongName = string;
+        classA.weakName = string;
+        NSLog(@"string:%@ address: %p",string,string);
+        
+        NSLog(@"copy Property:%@ address: %p",classA.name,classA.name);
+        
+        NSLog(@"strong Property:%@ address: %p",classA.strongName,classA.strongName);
+        
+        NSLog(@"weak Property:%@ address: %p",classA.weakName, classA.weakName);
     }
     return 0;
 }
