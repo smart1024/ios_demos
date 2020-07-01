78b7d0dfd7d034c8356ee09fc177c5dbf130de69
liling <1059232202@qq.com>
2020-07-01 09:24:12 +0800

    遵守UIScrollViewDelegate来监听UIScrollView的各种行为如滚动状态、子元素缩放等

--- 09_UIScrollView_01/09_UIScrollView_01/ViewController.m
+++ 09_UIScrollView_01/09_UIScrollView_01/ViewController.m
@@ -8,11 +8,18 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController ()<UIScrollViewDelegate>
 
 @end
 
 @implementation ViewController
+
+- (UIImageView *)imageView{
+    if (_imageView==nil) {
+        _imageView = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"image"]];
+    }
+    return _imageView;
+}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
@@ -31,10 +38,43 @@
     
     myScrollView.contentInset = UIEdgeInsetsMake(0, 0, 0, 0);
     
+    myScrollView.minimumZoomScale = 0.3;
+    
+    myScrollView.maximumZoomScale = 3;
+    
     [myScrollView addSubview:imageView];
     
     [self.view addSubview:myScrollView];
+    
+    myScrollView.delegate = self;
 }
 
+//scrollView滚动时不断调用
+- (void)scrollViewDidScroll:(UIScrollView *)scrollView{
+    NSLog(@"滚动时，调用scrollViewDidScroll:方法");
+}
+
+//scrollView即将开始滚动
+- (void)scrollViewWillBeginDragging:(UIScrollView *)scrollView{
+     NSLog(@"即将开始滚动时，调用scrollViewWillBeginDragging:方法");
+}
+-(void)scrollViewWillEndDragging:(UIScrollView *)scrollView withVelocity:(CGPoint)velocity targetContentOffset:(inout CGPoint *)targetContentOffset{
+    NSLog(@"停止滚动时，调用scrollViewWillEndDragging:方法");
+    NSLog(@"velocity x=%f,y=%f",velocity.x,velocity.y);
+    NSLog(@"targetContentOffset x=%f,y=%f",targetContentOffset->x,targetContentOffset->y);
+}
+
+- (void)scrollViewWillBeginZooming:(UIScrollView *)scrollView withView:(UIView *)view{
+    NSLog(@"图片即将开始缩放");
+}
+
+- (void)scrollViewDidEndZooming:(UIScrollView *)scrollView withView:(UIView *)view atScale:(CGFloat)scale{
+    NSLog(@"图片缩放完成");
+}
+
+//设置监听缩放view
+- (UIView *)viewForZoomingInScrollView:(UIScrollView *)scrollView{
+    return self.imageView;
+}
 
 @end
