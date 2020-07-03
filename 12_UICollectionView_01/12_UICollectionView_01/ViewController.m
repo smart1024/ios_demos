9885a8afbc0047e766d0fe06143a07b149daf664
liling <1059232202@qq.com>
2020-07-03 11:04:36 +0800

    给UITableView cell添加事件处理

--- 12_UICollectionView_01/12_UICollectionView_01/ViewController.m
+++ 12_UICollectionView_01/12_UICollectionView_01/ViewController.m
@@ -8,7 +8,7 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()<UICollectionViewDataSource>
+@interface ViewController ()<UICollectionViewDelegate,UICollectionViewDataSource>
 @property (nonatomic,strong) UICollectionView* collectionView;
 @end
 
@@ -20,6 +20,7 @@
         UICollectionViewFlowLayout* flowLayout = [[UICollectionViewFlowLayout alloc] init];
         _collectionView = [[UICollectionView alloc] initWithFrame:self.view.bounds collectionViewLayout:flowLayout];
         _collectionView.dataSource = self;
+        _collectionView.delegate = self;
         _collectionView.backgroundColor = [UIColor whiteColor];
     }
     return _collectionView;
@@ -28,6 +29,7 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     [self.view addSubview:self.collectionView];
+    //注册单元格，在懒加载方法中也可以
     [self.collectionView registerClass:[UICollectionViewCell class] forCellWithReuseIdentifier:@"cell"];
 }
 
@@ -36,9 +38,35 @@
     return 100;
 }
 
+//返回单元格
 - (UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath{
     UICollectionViewCell* cell = [collectionView dequeueReusableCellWithReuseIdentifier:@"cell" forIndexPath:indexPath];
     cell.backgroundColor = [UIColor greenColor];
     return cell;
 }
+
+//设置单元格能否被选中
+- (BOOL)collectionView:(UICollectionView *)collectionView shouldSelectItemAtIndexPath:(NSIndexPath *)indexPath{
+    return YES;
+}
+
+//设置单元格能否取消选中
+- (BOOL)collectionView:(UICollectionView *)collectionView shouldDeselectItemAtIndexPath:(NSIndexPath *)indexPath{
+    return YES;
+}
+
+//单元格选中时
+- (void)collectionView:(UICollectionView *)collectionView didSelectItemAtIndexPath:(NSIndexPath *)indexPath{
+    NSLog(@"%s",__func__);
+    UICollectionViewCell* cell = [collectionView cellForItemAtIndexPath:indexPath];
+    cell.backgroundColor = [UIColor blueColor];
+}
+
+//单元格取消选中时
+- (void)collectionView:(UICollectionView *)collectionView didDeselectItemAtIndexPath:(NSIndexPath *)indexPath{
+    NSLog(@"%s",__func__);
+    UICollectionViewCell* cell = [collectionView cellForItemAtIndexPath:indexPath];
+    cell.backgroundColor = [UIColor redColor];
+}
+
 @end
